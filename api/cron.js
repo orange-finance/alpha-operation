a0ebac212bec66fa9e85c951d3374cf386381a6d
shinobu1729 <sino1729defi@gmail.com>
2023-04-05 20:10:28 +0900

    fix

--- api/cron.js
+++ api/cron.js
@@ -2,7 +2,7 @@ const { main } = require("../src/main");
 
 module.exports = async (req, res) => {
   try {
-    await main();
+    await main.main();
     return res.status(200).send("Success");
   } catch (err) {
     console.error(err);
