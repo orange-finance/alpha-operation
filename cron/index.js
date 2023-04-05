66242f0254fbd088f0c6e23ed298f91b025934e7
shinobu1729 <sino1729defi@gmail.com>
2023-04-05 19:23:10 +0900

    fix cron

---
+++ cron/index.js
@@ -0,0 +1,11 @@
+const { main } = require("../src/main");
+
+module.exports = async (req, res) => {
+  try {
+    await main();
+    return res.status(200).send("Success");
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Error");
+  }
+};
